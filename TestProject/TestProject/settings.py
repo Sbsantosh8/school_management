e58c94b479accbe18cae245199f3d9ac3bc50947
Sbsantosh8 <santoshmudhiraj81@gmail.com>
2025-08-05 10:28:20 +0530

    created git ignore

--- TestProject/TestProject/settings.py
+++ TestProject/TestProject/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'TestProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',   # PostgreSQL backend
+        'NAME': 'school_management',                 # Database name
+        'USER': 'postgres',                          # Database user
+        'PASSWORD': 'root',                 # Database password
+        'HOST': 'localhost',                         # Or IP address / domain
+        'PORT': '5432',                               # Default PostgreSQL port
     }
 }
 
